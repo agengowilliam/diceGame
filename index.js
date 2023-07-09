ddcd2b44731592e9e9f03cf24d6398516a8d00cd
agengowilliam <137054173+agengowilliam@users.noreply.github.com>
2023-07-09 14:43:01 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+//console.log(randomNumber1);
+var randomDiceSource = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0].setAttribute("src",randomDiceSource );
+
+
+var randomNumber2 = Math.floor(Math.random()* 6) + 1;
+var randomDiceSource2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomDiceSource2);
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").textContent = "Player1 Wins!"
+} else if (randomNumber2 > randomNumber1){
+  document.querySelector("h1").textContent = "Player2 Wins!"
+} else {
+  document.querySelector("h1").textContent = "Draw!"
+}
